9636bf3e8647c8616bcd63e9d1dc3162a1709b25
liangzhujin <alfredtofu@gmail.com>
2018-09-03 21:06:31 +0800

    pretty code

--- interview_arrange.py
+++ interview_arrange.py
@@ -7,37 +7,13 @@ specify_final = False
 interviewers = None
 interviewees = None
 
+
 def parse_args():
     parser = argparse.ArgumentParser('校招面试安排')
     parser.add_argument('input_file', help='输入文件（excel文件，第一行面试官，第二行终面官，第三行面试者）')
     parser.add_argument('--start_time', help='开始时间', default='9:00')
     parser.add_argument('--interval_time', help='时间间隔（分钟）', default=30)
     return parser.parse_args()
-
-
-class Person(object):
-    def __init__(self, name, time_idx=0, used_lst=None, idx=-1):
-        self.name = name
-        self.item_idx = idx
-        self.time_idx = time_idx
-        self.used_lst = {} if used_lst is None else used_lst
-
-    def __str__(self):
-        return "name: %s, time_idx: %d, used_lst：%s" % (self.name, self.time_idx, str(self.used_lst))
-
-    def __lt__(self, other):
-        if len(self.used_lst) == 2 and len(self.used_lst) == len(other.used_lst) and specify_final:
-            return self.item_idx < other.item_idx
-        if len(interviewers) < len(interviewees[0]):
-            return self.item_idx < other.item_idx
-        if self.time_idx == other.time_idx:
-            # 如果时间一样，看目前时间是不是刚开始
-            # 如果刚开始，谁先来谁放前面
-            # 如果不是刚开始，谁先来谁放后面
-            return self.item_idx < other.item_idx if self.time_idx == 0 else self.item_idx > other.item_idx
-        return self.time_idx > other.time_idx
-
-    __repr__ = __str__
 
 
 def read_data(input_file):
@@ -47,16 +23,16 @@ def read_data(input_file):
     interviewees = [[], []]
     for i, interviewee in enumerate(input_book.col_values(0)):
         if interviewee:
-            interviewees[0].append(Person(interviewee))
+            interviewees[0].append(interviewee)
 
     for i, interviewee in enumerate(input_book.col_values(1)):
         if interviewee:
-            interviewees[1].append(Person(interviewee))
+            interviewees[1].append(interviewee)
 
     interviewers = []
     for i, interviewer in enumerate(input_book.col_values(2)):
         if interviewer:
-            interviewers.append(Person(interviewer, idx=i))
+            interviewers.append(interviewer)
 
 
 if __name__ == '__main__':
@@ -73,53 +49,32 @@ if __name__ == '__main__':
     sheet1.write(0, 0, '面试官')
     for i in range(len(interviewees)):
         for j in range(len(interviewees[i])):
-            sheet1.write(idx, 0, interviewees[i][j].name)
+            sheet1.write(idx, 0, interviewees[i][j])
             idx += 1
 
-    time_idx = 0
-    while len(interviewers) != 0:
-        while True:
-            has_delete = False
-            for i in range(len(interviewers)):
-                if interviewers[i].time_idx > time_idx:
-                    # 如果面试者在这个时间段已经被面过
-                    continue
-                if specify_final and len(interviewers[i].used_lst) == 2:
-                    for j in range(len(interviewees[1])):
-                        if interviewees[1][j].time_idx > time_idx:
-                            # 如果面试官在这个时间段已经面了人
-                            continue
-                        if interviewees[1][j].name in interviewers[i].used_lst:
-                            # 如果面试官已经面过这个人
-                            continue
-                        interviewees[1][j].time_idx = time_idx + 1
-                        interviewers[i].time_idx = time_idx + 1
-                        interviewers[i].used_lst[interviewees[1][j].name] = [len(interviewees[0]) + j, time_idx + 1]
-                        break
-                else:
-                    for j in range(len(interviewees[0])):
-                        if interviewees[0][j].time_idx > time_idx:
-                            # 如果面试官在这个时间段已经面了人
-                            continue
-                        if interviewees[0][j].name in interviewers[i].used_lst:
-                            # 如果面试官已经面过这个人
-                            continue
-
-                        interviewees[0][j].time_idx = time_idx + 1
-                        interviewers[i].time_idx = time_idx + 1
-                        interviewers[i].used_lst[interviewees[0][j].name] = [j, time_idx + 1]
-                        break
-                if len(interviewers[i].used_lst) == 3:
-                    for k, v in interviewers[i].used_lst.items():
-                        sheet1.write(v[0] + 1, v[1], interviewers[i].name)
-                    interviewers.remove(interviewers[i])
-                    has_delete = True
-                    break
-            if not has_delete:
-                break
-        time_idx += 1
-        interviewers = sorted(interviewers)
-    for i in range(1, time_idx + 1):
+    step = 2 if len(interviewees[1]) != 0 else 3
+    max_col = 0
+    cur_col = [0] * len(interviewees[1])
+    for i in range(len(interviewers)):
+        beg_x = int(i % len(interviewees[0]))
+        beg_y = int(i / len(interviewees[0])) * step + 1
+        sheet1.write(beg_x % len(interviewees[0]) + 1, beg_y, interviewers[i])
+        sheet1.write((beg_x + 1) % len(interviewees[0]) + 1, beg_y + 1, interviewers[i])
+        if step == 2:
+            beg_x_f = int(i % len(interviewees[1]))
+            beg_y_f = int(i / len(interviewees[1]))
+            if beg_y_f == 0:
+                cur_col[beg_x_f] = int(i / len(interviewees[1])) + beg_y + 1 + 1
+            else:
+                cur_col[beg_x_f] = max(cur_col[beg_x_f] + 1, beg_y + 1 + 1)
+            sheet1.write(beg_x_f + len(interviewees[0]) + 1,
+                         cur_col[beg_x_f],
+                         interviewers[i])
+            max_col = cur_col[beg_x_f]
+        else:
+            max_col = beg_y + 2
+            sheet1.write((beg_x + 2) % len(interviewees[0]) + 1, max_col, interviewers[i])
+    for i in range(1, max_col):
         sheet1.write(0, i,
                      (start_time + timedelta(minutes=(i - 1) * args.interval_time)).time().strftime('%H:%M') + '-' +
                      (start_time + timedelta(minutes=i * args.interval_time)).time().strftime('%H:%M'))
